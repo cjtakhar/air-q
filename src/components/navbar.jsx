97aba4842d9acba2e40cbda43c06d90241a4299a
cjtakhar <cjtakhar@gmail.com>
2023-05-21 17:21:03 -0700

    css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,13 +6,6 @@ function NavBar() {
   return (
     <Navbar className="navBar" expand="lg">
       <Navbar.Brand className="navBrand" href="/">Air Q</Navbar.Brand>
-      <Navbar.Toggle className="navToggle" aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse className="navCollapse" id="basic-navbar-nav">
-        {/* <Nav className="nav-links">
-          <Nav.Link className="nav-link" href="/account">Account</Nav.Link>
-          <Nav.Link className="nav-link" href="/about">About</Nav.Link>
-        </Nav> */}
-      </Navbar.Collapse>
     </Navbar>
   );
 }
